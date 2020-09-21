2ee05149ad868c925c6da69a4b05a5b0613c7581
DESKTOP-K0HS5QI\Dell <vaideska@mail.ru>
2020-09-21 19:39:37 +0300

    adding options

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -4,5 +4,7 @@ const { program } = require('commander');
 program.description('Compares two configuration files and shows a difference.');
 program.helpOption('-h, --help', 'read more information');
 program.version('0.0.1', '-v, --version', 'output the version number');
+program.arguments('<filepath1>,<filepath2>');
+program.option('-f, --format [type]', 'output format');
 
 program.parse(process.argv);
