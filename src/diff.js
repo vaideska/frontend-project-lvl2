98ac4de5bbed6de5b3f76caa65af707db6b4478c
DESKTOP-K0HS5QI\Dell <vaideska@mail.ru>
2020-10-29 00:37:39 +0300

    add normal path

--- src/diff.js
+++ src/diff.js
@@ -1,13 +1,18 @@
 import fs from 'fs';
+import path from 'path';
+import process from 'process';
+
+const getNormalPath = (filepath) => path.resolve(process.cwd(), filepath);
 
 const diffFiles = (filepath1, filepath2) => {
+  console.log(getNormalPath(filepath1));
   /* const jsonText1 = fs.readFileSync
   ('C:\\programming\\frontend-project-lvl2\\test\\file1.JSON', 'utf8');
   const jsonText2 = fs.readFileSync
   ('C:\\programming\\frontend-project-lvl2\\test\\file2.JSON', 'utf8'); */
 
-  const jsonText1 = fs.readFileSync(filepath1, 'utf8');
-  const jsonText2 = fs.readFileSync(filepath2, 'utf8');
+  const jsonText1 = fs.readFileSync(getNormalPath(filepath1), 'utf8');
+  const jsonText2 = fs.readFileSync(getNormalPath(filepath2), 'utf8');
 
   const jsonObj1 = JSON.parse(jsonText1);
   const jsonObj2 = JSON.parse(jsonText2);
