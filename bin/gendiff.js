fa92bda9b5382a0cf1d54c7b8f595a5187ae8bcc
DESKTOP-K0HS5QI\Dell <vaideska@mail.ru>
2020-11-02 19:03:42 +0300

    Comparing flat files (JSON)

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -5,8 +5,8 @@ import diffFiles from '../src/diff.js';
 program.description('Compares two configuration files and shows a difference.');
 program.helpOption('-h, --help', 'read more information');
 program.version('0.0.1', '-v, --version', 'output the version number');
-program.arguments('<filepath1>,<filepath2>');
+program.arguments('<filepath1> <filepath2>');
 program.option('-f, --format [type]', 'output format');
-program.action((filepath1, filepath2) => diffFiles(filepath1, filepath2));
+program.action((filepath1, filepath2) => console.log(diffFiles(filepath1, filepath2)));
 
 program.parse(process.argv);
--- src/diff.js
+++ src/diff.js
@@ -1,25 +1,44 @@
 import fs from 'fs';
 import path from 'path';
-import process from 'process';
+import _ from 'lodash';
 
 const getNormalPath = (filepath) => path.resolve(process.cwd(), filepath);
 
 const diffFiles = (filepath1, filepath2) => {
-  console.log(getNormalPath(filepath1));
-  /* const jsonText1 = fs.readFileSync
-  ('C:\\programming\\frontend-project-lvl2\\test\\file1.JSON', 'utf8');
-  const jsonText2 = fs.readFileSync
-  ('C:\\programming\\frontend-project-lvl2\\test\\file2.JSON', 'utf8'); */
-
   const jsonText1 = fs.readFileSync(getNormalPath(filepath1), 'utf8');
   const jsonText2 = fs.readFileSync(getNormalPath(filepath2), 'utf8');
 
   const jsonObj1 = JSON.parse(jsonText1);
   const jsonObj2 = JSON.parse(jsonText2);
 
-  console.log(jsonObj1, jsonObj2);
-};
+  const getResultObj1 = (acc, key) => {
+    const value = jsonObj1[key];
+    let res = acc;
+    if (_.has(jsonObj2, key)) {
+      if (value === jsonObj2[key]) res += `    ${key}: ${value}\n`;
+      else res += `  - ${key}: ${value}\n  + ${key}: ${jsonObj2[key]}\n`;
+    } else res += `  - ${key}: ${value}\n`;
+    return res;
+  };
 
-//  diffFiles('C:\\programming\\test\\file1.JSON', 'C:\\programming\\test\\file2.JSON');
+  const getResultObj2 = (acc, key) => {
+    const value = jsonObj2[key];
+    let res = acc;
+    if (!(_.has(jsonObj1, key))) res += `  + ${key}: ${value}\n`;
+    return res;
+  };
+
+  let result = '{\n';
+  let keysObj1 = Object.keys(jsonObj1);
+  let keysObj2 = Object.keys(jsonObj2);
+  keysObj1 = keysObj1.sort();
+  keysObj2 = keysObj2.sort();
+  result = keysObj1.reduce(getResultObj1, result);
+  result = keysObj2.reduce(getResultObj2, result);
+
+  result += '}';
+
+  return result;
+};
 
 export default diffFiles;
